5936a172728d9d2819bc74ef387406cfd975374c
Mariya Patrusheva <mpatrusheva@lifedl.net>
2025-05-09 19:05:46 +0400

    fix loader

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -104,168 +104,170 @@ export const MainPage = () => {
       <video autoPlay muted loop playsInline id="myVideo">
         <source src={videoBackground} type="video/mp4" />
       </video>
-      <div className="main">
-        <div className="main_start">
-          <div className="invitation">приглашение на свадьбу</div>
-          <Names />
-          <div className="date">сентябрь</div>
-          <div className="date_number">06</div>
-          <div className="date">2025</div>
-          <FlexCenter
-            className="arrow"
-            initial={{ opacity: 0 }}
-            animate={{ y: [0, 20, 0], opacity: 1 }}
-            transition={{
-              delay: 1.5,
-              duration: 4,
-              times: [0, 0.5, 1],
-              repeat: Infinity,
-            }}
-          >
-            <Arrow />
-          </FlexCenter>
-        </div>
-
-        <div className="main_all">
-          <motion.div
-            variants={textVariants}
-            initial="offscreen"
-            whileInView="onscreen"
-            viewport={{ once: true, amount: 1 }}
-            className="text"
-          >
-            «Счастье – это когда ты нашел того, кто искал тебя». <br /> Этот
-            день станет началом нашей долгой и счастливой семейной жизни.
-          </motion.div>
-          <motion.div
-            variants={textVariants}
-            initial="offscreen"
-            whileInView="onscreen"
-            viewport={{ once: true, amount: 1 }}
-            className="text"
-            style={{ fontWeight: 600, marginLeft: "6px" }}
-          >
-            Мы приглашаем Вас на торжество, посвященное нашей свадьбе.
-          </motion.div>
-          <div className="month">Сентябрь</div>
-          <div className="month_container">
-            <motion.div
-              className="wedding_day"
-              initial={{ scale: 1 }}
-              animate={{ scale: 1.2 }}
+      {!loader && (
+        <div className="main">
+          <div className="main_start">
+            <div className="invitation">приглашение на свадьбу</div>
+            <Names />
+            <div className="date">сентябрь</div>
+            <div className="date_number">06</div>
+            <div className="date">2025</div>
+            <FlexCenter
+              className="arrow"
+              initial={{ opacity: 0 }}
+              animate={{ y: [0, 20, 0], opacity: 1 }}
               transition={{
+                delay: 1.5,
+                duration: 4,
+                times: [0, 0.5, 1],
                 repeat: Infinity,
-                repeatType: "reverse",
-                duration: 1.5,
               }}
             >
-              <Heart />
+              <Arrow />
+            </FlexCenter>
+          </div>
+
+          <div className="main_all">
+            <motion.div
+              variants={textVariants}
+              initial="offscreen"
+              whileInView="onscreen"
+              viewport={{ once: true, amount: 1 }}
+              className="text"
+            >
+              «Счастье – это когда ты нашел того, кто искал тебя». <br /> Этот
+              день станет началом нашей долгой и счастливой семейной жизни.
+            </motion.div>
+            <motion.div
+              variants={textVariants}
+              initial="offscreen"
+              whileInView="onscreen"
+              viewport={{ once: true, amount: 1 }}
+              className="text"
+              style={{ fontWeight: 600, marginLeft: "6px" }}
+            >
+              Мы приглашаем Вас на торжество, посвященное нашей свадьбе.
+            </motion.div>
+            <div className="month">Сентябрь</div>
+            <div className="month_container">
+              <motion.div
+                className="wedding_day"
+                initial={{ scale: 1 }}
+                animate={{ scale: 1.2 }}
+                transition={{
+                  repeat: Infinity,
+                  repeatType: "reverse",
+                  duration: 1.5,
+                }}
+              >
+                <Heart />
+              </motion.div>
+
+              {month.map((week, index) => (
+                <div
+                  className="week_container"
+                  style={{ justifyContent: index === 0 ? "end" : "start" }}
+                >
+                  {week.map((day) => (
+                    <div
+                      style={{
+                        width: "28px",
+                      }}
+                    >
+                      {day}
+                    </div>
+                  ))}
+                </div>
+              ))}
+            </div>
+            <Divider className="divider" />
+            <motion.div
+              variants={titleVariants}
+              initial="offscreen"
+              whileInView="onscreen"
+              viewport={{ once: true, amount: 1 }}
+              className="title"
+            >
+              Место проведения
+            </motion.div>
+            <motion.div
+              variants={textVariants}
+              initial="offscreen"
+              whileInView="onscreen"
+              viewport={{ once: true, amount: 1 }}
+              className="text"
+            >
+              г. Ульяновск, ул. Спуск Степана Разина, 33 <br />
+              Ресторанный комплекс «Ленинские горки»
+            </motion.div>
+            <div className="text">СБОР ГОСТЕЙ</div>
+            <div
+              className="text"
+              style={{
+                fontWeight: 500,
+                fontSize: "24px",
+                marginTop: "16px",
+              }}
+            >
+              Время сообщим позже
+            </div>
+
+            <motion.div
+              variants={locationVariants}
+              initial="offscreen"
+              whileInView="onscreen"
+              viewport={{ once: true, amount: 1 }}
+            >
+              <Location
+                style={{ width: "100px", height: "120px", marginTop: "20px" }}
+              />
             </motion.div>
 
-            {month.map((week, index) => (
-              <div
-                className="week_container"
-                style={{ justifyContent: index === 0 ? "end" : "start" }}
-              >
-                {week.map((day) => (
-                  <div
-                    style={{
-                      width: "28px",
-                    }}
-                  >
-                    {day}
-                  </div>
-                ))}
-              </div>
-            ))}
-          </div>
-          <Divider className="divider" />
-          <motion.div
-            variants={titleVariants}
-            initial="offscreen"
-            whileInView="onscreen"
-            viewport={{ once: true, amount: 1 }}
-            className="title"
-          >
-            Место проведения
-          </motion.div>
-          <motion.div
-            variants={textVariants}
-            initial="offscreen"
-            whileInView="onscreen"
-            viewport={{ once: true, amount: 1 }}
-            className="text"
-          >
-            г. Ульяновск, ул. Спуск Степана Разина, 33 <br />
-            Ресторанный комплекс «Ленинские горки»
-          </motion.div>
-          <div className="text">СБОР ГОСТЕЙ</div>
-          <div
-            className="text"
-            style={{
-              fontWeight: 500,
-              fontSize: "24px",
-              marginTop: "16px",
-            }}
-          >
-            Время сообщим позже
-          </div>
+            <button className="button_location" onClick={handleClickLocation}>
+              Как добраться?
+            </button>
+            <motion.div
+              variants={titleVariants}
+              initial="offscreen"
+              whileInView="onscreen"
+              viewport={{ once: true, amount: 1 }}
+              className="title"
+            >
+              Подсказки
+            </motion.div>
 
-          <motion.div
-            variants={locationVariants}
-            initial="offscreen"
-            whileInView="onscreen"
-            viewport={{ once: true, amount: 1 }}
-          >
-            <Location
-              style={{ width: "100px", height: "120px", marginTop: "20px" }}
+            <Children
+              className="children"
+              style={{ marginTop: "20px", " g": { fill: "#3f352b" } }}
             />
-          </motion.div>
+            <motion.div
+              variants={textVariants}
+              initial="offscreen"
+              whileInView="onscreen"
+              viewport={{ once: true, amount: 1 }}
+              className="text"
+            >
+              Наша свадьба - праздник для взрослых. <br /> Пожалуйста,
+              позаботьтесь о том, чтобы оставить детишек дома под любящим
+              присмотром и всецело насладитесь атмосферой нашего торжества.
+            </motion.div>
+            <Divider className="divider" />
 
-          <button className="button_location" onClick={handleClickLocation}>
-            Как добраться?
-          </button>
-          <motion.div
-            variants={titleVariants}
-            initial="offscreen"
-            whileInView="onscreen"
-            viewport={{ once: true, amount: 1 }}
-            className="title"
-          >
-            Подсказки
-          </motion.div>
+            <motion.div
+              variants={locationVariants}
+              initial="offscreen"
+              whileInView="onscreen"
+              viewport={{ once: true, amount: 1 }}
+              className="title"
+              style={{ marginBottom: "60px", fontFamily: "Denistina, serif" }}
+            >
+              С любовью, <br /> Александр и Екатерина
+            </motion.div>
 
-          <Children
-            className="children"
-            style={{ marginTop: "20px", " g": { fill: "#3f352b" } }}
-          />
-          <motion.div
-            variants={textVariants}
-            initial="offscreen"
-            whileInView="onscreen"
-            viewport={{ once: true, amount: 1 }}
-            className="text"
-          >
-            Наша свадьба - праздник для взрослых. <br /> Пожалуйста,
-            позаботьтесь о том, чтобы оставить детишек дома под любящим
-            присмотром и всецело насладитесь атмосферой нашего торжества.
-          </motion.div>
-          <Divider className="divider" />
-
-          <motion.div
-            variants={locationVariants}
-            initial="offscreen"
-            whileInView="onscreen"
-            viewport={{ once: true, amount: 1 }}
-            className="title"
-            style={{ marginBottom: "60px", fontFamily: "Denistina, serif" }}
-          >
-            С любовью, <br /> Александр и Екатерина
-          </motion.div>
-
-          <CountDown date={new Date("Sat, 6 Sep 2025 00:00:00")} />
+            <CountDown date={new Date("Sat, 6 Sep 2025 00:00:00")} />
+          </div>
         </div>
-      </div>
+      )}
     </>
   );
 };
